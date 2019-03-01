80f059a72e99df3300fae0998ae3b5b3772adb5d
Keegan Stothert <keeganstothert@gmail.com>
2019-03-01 18:52:12 +0000

    fix airborne and grounded systems

--- Assets/Source/Systems/CharacterController/GroundedSystem.cs
+++ Assets/Source/Systems/CharacterController/GroundedSystem.cs
@@ -4,11 +4,11 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-namespace RocketJump{
-  public class GroundedSystem:ComponentSystem{
+namespace RocketJump {
+  public class GroundedSystem : ComponentSystem {
     ComponentGroup grounded;
 
-    protected override void OnCreateManager(){
+    protected override void OnCreateManager () {
       grounded = GetComponentGroup(
         ComponentType.Subtractive(typeof(Grounded)),
         typeof(Player),
@@ -16,17 +16,13 @@ namespace RocketJump{
       );
     }
 
-    protected override void OnUpdate(){
+    protected override void OnUpdate () {
       var g_player = grounded.GetEntityArray();
       var g_collision = grounded.GetComponentDataArray<CollidedWithGround>();
-      
-      for(int i = 0; i < grounded.CalculateLength(); i++){
-        if(g_collision[i].Impact.State != State.Overlap.Enter)
-          continue;
 
-        PostUpdateCommands.AddComponent<Grounded>(g_player[i], new Grounded{});
+      for (int i = 0; i < grounded.CalculateLength(); i++) {
+        PostUpdateCommands.AddComponent<Grounded>(g_player[i], new Grounded { });
 
-        
         // Damage Example:
         // EntityManager.GetComponentData<Damage>(g_collision[i].Impact.OtherEntity);
       }
