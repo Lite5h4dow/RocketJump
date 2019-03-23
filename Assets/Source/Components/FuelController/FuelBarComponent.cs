ce8b18bd86a31e4b2e63835a5e29be48ebec3309
Shadow-Dev <Lite5h4dow@users.noreply.github.com>
2019-03-23 15:42:07 +0000

    fuel bar addition

---
+++ Assets/Source/Components/FuelController/FuelBarComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace RocketJump{
+  [Serializable]
+  public struct FuelBar:IComponentData{}
+  public class FuelBarComponent : ComponentDataProxy<FuelBar>{}
+}
---
+++ Assets/Source/Systems/FuelBarController/FuelBarRocketActiveSystem.cs
@@ -0,0 +1,42 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace RocketJump{
+  public class FuelBarRocketActiveSystem:ComponentSystem{
+    ComponentGroup fuel;
+    ComponentGroup player;
+
+    protected override void OnCreateManager(){
+      fuel = GetComponentGroup(
+        typeof(FuelBar),
+        typeof(Slider)
+      );
+
+      player = GetComponentGroup(
+        typeof(Player),
+        typeof(MaxRocketFuel),
+        typeof(RocketActive)
+      );
+
+      RequireForUpdate(fuel);
+      RequireForUpdate(player);
+    }
+
+    protected override void OnUpdate(){
+      var p_entity = player.GetEntityArray();
+      var f_entity = fuel.GetEntityArray();
+      var p_fuel = player.GetComponentDataArray<RocketActive>();
+      var p_maxFuel = player.GetComponentDataArray<MaxRocketFuel>();
+      var f_value = fuel.GetComponentArray<Slider>();
+      
+      for(int i = 0; i < fuel.CalculateLength(); i++){
+        f_value[i].value = p_fuel[i].Value / p_maxFuel[i].Value;
+
+      }
+    }
+  }
+}
